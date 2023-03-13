de9de27dfc8865a887cfde800c70ea0bd54ee5c1
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2023-03-13 23:07:55 +0300

    edit ui

--- app/src/main/java/com/example/programmercalculator/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculator/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.programmercalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    lateinit var spinner: Spinner
+    lateinit var spinner2: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val spinner: Spinner = findViewById(R.id.spinner)
-        val spinner2: Spinner = findViewById(R.id.spinner2)
+        initSpinner()
+    }
+    private fun initSpinner(){
+        spinner= findViewById(R.id.spinner)
+        spinner2= findViewById(R.id.spinner2)
 
         ArrayAdapter.createFromResource(
             this,
