53f8cc54645ca4de721343f5d0575246814e2edf
Karrar Abbas <54599574+karrar-abbas@users.noreply.github.com>
2023-03-14 19:38:02 +0300

    add logic part

--- app/src/main/java/com/example/programmercalculator/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculator/MainActivity.kt
@@ -2,18 +2,36 @@ package com.example.programmercalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.ButtonBarLayout
 
 class MainActivity : AppCompatActivity() {
     lateinit var spinner: Spinner
     lateinit var spinner2: Spinner
+    lateinit var values:TextView
+    lateinit var result:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initview()
         initSpinner()
+    }
+    private fun initview(){
+        values=findViewById(R.id.firstNum)
+    }
+    fun clear(v:View){
+       values.text=""
+    }
+    fun res(v:View){
+
+        val decimal = Integer.valueOf(values.text.toString(),16)
+        result.text=decimal.toString()
     }
     private fun initSpinner(){
         spinner= findViewById(R.id.spinner)
@@ -39,5 +57,11 @@ class MainActivity : AppCompatActivity() {
 
             spinner2.adapter = adapter
         }
+    }
+    fun onClickButton(v:View){
+        val newEnter=(v as Button).text.toString()
+        val oldEnter=values.text.toString()
+        val newAdd=oldEnter+newEnter
+        values.text=newAdd
     }
 }
